c9bc6db8c6bab4af048cacc9457b85f49a94b210
Verik Alexander <suolucitem@gmail.com>
2021-04-26 12:24:09 +0300

    Working with IDE

--- M01_BasicCoding/ArrayHelper/ArrayHelper/Array2DSum.cs
+++ M01_BasicCoding/ArrayHelper/ArrayHelper/Array2DSum.cs
@@ -1,7 +1,7 @@
-using System.Linq;
+﻿using System.Linq;
 namespace ArrayHelper {
     public static class Array2DSum {
-        public static int SumOfPotistivesIn2dArray(int[,] array) {
+        public static int SumOfPositivesIn2dArray(int[,] array) {
             if (array == null)
                 throw new System.NullReferenceException();
             return (from int value in array where (value > 0) select value).Sum();
