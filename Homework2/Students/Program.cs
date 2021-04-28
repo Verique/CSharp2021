b787b44dfbedc5ff986c7c5a39d9fe2998863675
Verik Alexander <suolucitem@gmail.com>
2021-04-28 11:53:16 +0300

    Done Task 1

---
+++ Homework2/Students/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+
+namespace Students
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var subjects = new string[] {"Maths", "PE", "Chemistry", "IT", "Literature", "French"};
+            var student1c1 = new Student("alexander.verik@epam.com");
+            var student2c1 = new Student("denis.kozak@epam.com");
+            var student3c1 = new Student("elena.smirnova@epam.com");
+            var student1c2 = new Student("Alexander", "Verik");
+            var student2c2 = new Student("Denis", "Kozak");
+            var student3c2 = new Student("Elena", "Smirnova");
+
+            var dict = new Dictionary<Student, HashSet<string>>
+            {
+                [student1c1] = GetRandomSubjects(subjects),
+                [student1c2] = GetRandomSubjects(subjects),
+                [student2c1] = GetRandomSubjects(subjects),
+                [student2c2] = GetRandomSubjects(subjects),
+                [student3c1] = GetRandomSubjects(subjects),
+                [student3c2] = GetRandomSubjects(subjects),
+            };
+
+            Console.WriteLine("Dictionary contains {0} students : ", dict.Keys.Count);
+            foreach (var student in dict.Keys)
+            {
+                Console.WriteLine(student.ToString());
+            }
+
+        }
+
+        private static HashSet<string> GetRandomSubjects(string[] subjects)
+        {
+            var rnd = new Random();
+            var max = subjects.Length - 1;
+            var result = new HashSet<string>();
+            
+            for (var i = 0; i < 3; i++)
+                result.Add(subjects[rnd.Next(max)]);
+
+            return result;
+        }
+    }
+}
---
+++ Homework2/Students/Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace Students
+{
+    public class Student
+    {
+        private readonly string _fullName;
+        private readonly string _email;
+
+        public Student(string email)
+        {
+            if ((!email.Contains("@")) || (!email.Contains('.')))
+                throw new ArgumentException();
+            if (email.IndexOf('.') > email.IndexOf('@'))
+                throw new ArgumentException();
+            this._email = email;
+            _fullName = GetNameFromEmail(email);
+        }
+
+        public Student(string name, string surname)
+        {
+            _email = string.Concat(FirstLetterToLower(name), '.', FirstLetterToLower(surname), "@epam.com");
+            _fullName = string.Concat(name, ' ', surname);
+        }
+
+        public override string ToString()
+        {
+            return _fullName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Student student))
+                return false;
+            return this.GetHashCode().Equals(student.GetHashCode());
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
+        private string GetNameFromEmail(string email)
+        {
+            var nameAndSurname = email.Remove(email.IndexOf('@')).Split('.');
+            return string.Concat(FirstLetterToUpper(nameAndSurname[0]), ' ', FirstLetterToUpper(nameAndSurname[1]));
+        }
+
+        private string FirstLetterToUpper(string input)
+        {
+            return input.First().ToString().ToUpper() + input.Substring(1);
+        }
+
+        private string FirstLetterToLower(string input)
+        {
+            return input.First().ToString().ToLower() + input.Substring(1);
+        }
+    }
+}
